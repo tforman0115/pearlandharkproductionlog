9b6508ba7aaf6ce3abc38bf363d84a32e08738a2
Tyler Forman <tforman0115@gmail.com>
2024-11-28 01:49:57 -0500

    Added ping

--- routes/main.js
+++ routes/main.js
@@ -5,7 +5,12 @@ const router = express.Router();
 // When the bot sees a message in a group chat, this route is called
 router.post('/', (req, res, next) => {
     console.log(req.body)
-    res.send(200).json()
+    res.status(200).json()
+});
+
+// Test for pinging the bot
+router.get('/', (req, res, next) => {
+    res.status(200).json({"message": "Pong!"})
 });
 
 
